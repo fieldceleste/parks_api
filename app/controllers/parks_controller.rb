ef131dfaf50b09793b5ef6fb8bec963c90cfcf14
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-12 09:36:01 -0700

    add users and parks table

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,46 @@
+class ParksController < ApplicationController
+
+  def index 
+    @parks = Park.all
+    json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def search
+    @parks = Park.search_by_term(params[:query])
+    json_response(@parks)
+  end
+
+  def create 
+    @park =Park.create!(park_params)
+    json_response(@park, :created)
+  end
+  
+  def update
+    @park =Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "This park was updated successfully."
+      }
+    end
+  end
+
+  def destroy 
+    @park =Park.find(params[:id])
+    if @park.destroy
+      render status: 200, json: {
+        message: "This Park has been successfully deleted!"
+      }
+    end
+  end
+
+  private
+
+  def park_params
+    params.permit(:name, :state, :description, :established)
+  end
+end
---
+++ db/migrate/20200612162821_add_parks_table.rb
@@ -0,0 +1,10 @@
+class AddParksTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parks do |t|
+      t.column :name, :string
+      t.column :state, :string
+      t.column :description, :string
+      t.column :established, :integer
+    end
+  end
+end
