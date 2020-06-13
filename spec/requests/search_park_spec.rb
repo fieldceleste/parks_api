56547d2d5e20851e6318da60bf5f55e5c157f073
Celeste Field <44860174+fieldceleste@users.noreply.github.com>
2020-06-12 17:04:32 -0700

    pass second search spec

--- spec/requests/search_park_spec.rb
+++ spec/requests/search_park_spec.rb
@@ -11,4 +11,11 @@ describe "get all park routes", :type => :request do
     get '/search?query=3363'
     expect(JSON.parse(response.body).size).to eq(1)
   end
+
+  it 'returns error message when search result is empty' do
+    @park = Park.create( {:id => 2, :name => 'Rivendell', :description => '3 wolf moon pop-up tote bag ramps meditation gentrify chia fixie normcore selvage.', :state=> 'Michigan', :established => 3363, :national => false })
+    get '/search?query=12345677'
+    expect(response).to have_http_status(200)
+    expect(JSON.parse(response.body).size).to eq(0)
+  end
 end
